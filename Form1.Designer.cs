b676ebd144263a42c41ea6a09c6506eee3cd3c4f
Prashant V <prashant@prashantv.com>
2010-11-28 00:11:18 +1100

    Initial commit for iTunes -> Foobar mass tagger format

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace iTunesToFoobar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.prog = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtOut.Location = new System.Drawing.Point(0, 29);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(439, 505);
+            this.txtOut.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(811, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Output";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(445, 29);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(366, 505);
+            this.txtLog.TabIndex = 2;
+            // 
+            // prog
+            // 
+            this.prog.Dock = System.Windows.Forms.DockStyle.Top;
+            this.prog.Location = new System.Drawing.Point(0, 0);
+            this.prog.Name = "prog";
+            this.prog.Size = new System.Drawing.Size(811, 23);
+            this.prog.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 557);
+            this.Controls.Add(this.prog);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOut);
+            this.Name = "Form1";
+            this.Text = "iTunes To Mass Tagger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.ProgressBar prog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using iTunesLib;
+
+namespace iTunesToFoobar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void notFoundiTunes(IITFileOrCDTrack ft) {
+            txtLog.Text = "Could not find file: " + ft.Artist + ";" + ft.Album + ";" + ft.Name + "=\r\n" + txtLog.Text;
+        }
+
+        private void notFoundM3U(string line)
+        {
+            txtLog.Text = "Could not find file in iTunes with location: " + line + "\r\n" + txtLog.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, IITFileOrCDTrack> maps = new Dictionary<string,IITFileOrCDTrack>();
+
+            string m3uLoc;
+            openFileDialog1.Filter = "M3U Playlists|*.m3u";
+            openFileDialog1.CheckFileExists = true;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                m3uLoc = openFileDialog1.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Please select a file!");
+                return;
+            }
+            
+            iTunesApp iT = new iTunesApp();
+            IITTrackCollection tCol = iT.LibraryPlaylist.Tracks;
+            prog.Maximum = tCol.Count;
+            for(int i = 1; i <= tCol.Count; i++) {
+                IITTrack t = iT.LibraryPlaylist.Tracks[i];
+                if (t.Kind == ITTrackKind.ITTrackKindFile)
+                {
+                    IITFileOrCDTrack ft = (IITFileOrCDTrack)t;
+                    string location = ft.Location;
+                    if(location == null || location == "" ){
+                        notFoundiTunes(ft);
+                    } else {
+                        location = location.ToLower();
+                        maps[location] = ft;
+                    }
+
+                }
+                prog.Value = i;
+                prog.Refresh();
+            }
+
+            // and now iterate through them and print them
+            string[] lines = System.IO.File.ReadAllLines(m3uLoc);
+            prog.Maximum = lines.Length;
+            prog.Value = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (string l in lines)
+            {
+                string line = l.ToLower();
+                if(maps.ContainsKey(line)) {
+                    sb.AppendLine(getOutput(maps[line]));
+                } else {
+                    sb.AppendLine(getDefOutput());
+                    notFoundM3U(line);
+                }
+                prog.Value += 1;
+            }
+
+            txtOut.Text = sb.ToString();
+        }
+
+
+        private string getDefOutput()
+        {
+            // default output to use when we can't find the track in iTunes
+            return "0|0||0||";
+        }
+
+        private string getDateString(DateTime dt)
+        {
+            //"YYYY-MM-DD hh:mm:ss" is the format used by foobar2000
+            return dt.ToString("yyyy'-'MM'-'dd HH':'mm':'ss");
+        }
+
+        private string getOutput(IITFileOrCDTrack ft)
+        {
+            // format to use in mass tagger
+            // %rating%|%play_count%|%last_played%|%skip_count%|%last_skipped%|%added%
+
+            string added = "";
+            added = (ft.Rating / 20).ToString();
+            added += "|" + ft.PlayedCount;
+            added += "|" + getDateString(ft.PlayedDate) + "|" + ft.SkippedCount + "|" + getDateString(ft.SkippedDate) + "|" + getDateString(ft.DateAdded);
+            return added;
+        }
+    }
+}
